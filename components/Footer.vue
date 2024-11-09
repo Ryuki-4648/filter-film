d372156faf23f953d7d47b16e2e2bbf4100b70f2
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-11-10 01:31:26 +0900

    tailwind関連のエラーありver変更、コンポーネン ト作成

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer class="l-footer">
+    <p class="l-footerText"></p>
+    <p class="l-footerText">filter-film since 2024</p>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <header class="l-header">
+    <div class="l-header__inner">
+      <div class="l-header__index">
+        <img src="/img_logo01.png" :alt="`t('SITE_TITLE')`" class="l-header__logo" />
+        <h1 class="l-header__title"></h1>
+      </div>
+      <ul class="l-header__lang">
+        <li class="l-header__langItem">
+          <NuxtLink to="/en" class="l-header__langLink">
+            EN
+          </NuxtLink>
+        </li>
+        <li class="l-header__langItem">
+          <NuxtLink to="/" class="l-header__langLink">
+            JP
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
